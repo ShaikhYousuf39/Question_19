ec2b9a85150936aabbe1571da4c215729ebcb98e
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:15:02 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,4 @@
+"use strict";
+let invitedPeoples = ["Faizan", "Mohsin"];
+let count_invitedPeoples = invitedPeoples.length;
+console.log(`${count_invitedPeoples} peoples are invited in dinner`);
---
+++ new.ts
@@ -0,0 +1,4 @@
+let invitedPeoples: string[] = ["Faizan","Mohsin"]
+let count_invitedPeoples : number = invitedPeoples.length
+
+console.log(`${count_invitedPeoples} peoples are invited in dinner`);
